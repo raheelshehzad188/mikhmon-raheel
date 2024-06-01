4a2d80a30cd9dcf43d1577f1383e5ab999a3e6f1
Raheel shehzad <raheelshehzad188@gmail.com>
2024-06-02 03:47:08 +0500

    Attach database

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-define('BASE_URL','http://localhost/mikhmon/');
+define('BASE_URL','http://localhost/mikhmon-raheel/');
 define('DB_HOST','localhost');
 define('DB_NAME','mikhmon');
 define('DB_USER','root');
